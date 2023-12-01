743c92627730a5b4d2577288f855e6717e901518
NghiemthinhV <150453342+NghiemthinhV@users.noreply.github.com>
2023-12-01 14:19:50 +0700

    welcome

--- app/src/main/java/com/haocg/ourduan1nhom8/Welcome.java
+++ app/src/main/java/com/haocg/ourduan1nhom8/Welcome.java
@@ -2,13 +2,29 @@ package com.haocg.ourduan1nhom8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 
 public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
+
+        ImageView imgLogo = findViewById(R.id.imgChao);
+        Glide.with(this).load(R.drawable.book).into(imgLogo);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this,DangNhap.class);
+                startActivity(intent);
+            }
+        },5000);
     }
 }
